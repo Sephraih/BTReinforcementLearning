4f5b93a852bd830f851dd0fd9c425bfd9c0b4515
Shadowlink <Sordstriker_@hotmail.com>
2020-02-19 18:27:37 +0100

    Simplify the game (1)
    Enemy bots removed,
    Prefabs removed,
    Camera Follow script updated to have only a player and an enemy,
    Player bots removed

--- Legend of Sephraih (BT)/Assets/Scripts/camerafollow.cs
+++ Legend of Sephraih (BT)/Assets/Scripts/camerafollow.cs
@@ -9,8 +9,6 @@ public class camerafollow : MonoBehaviour
 
     //the three defined player characters
     public Transform ichi; // the first character, the dps
-    public Transform ni; // the second character, the healer
-    public Transform san; // the third character, the tank
     public Transform dummy; //dummy target to see that there is no enemy
 
     public Vector3 offset;
@@ -27,33 +25,9 @@ public class camerafollow : MonoBehaviour
     private void Update()
     {
 
-
-
-        if (Input.GetButtonDown("Swap"))
-        {
-            if (target == ichi)
-            {
-                //target.gameObject.SetActive(false);
-                target = ni;
-            }
-            else if (target == ni)
-            {
-                // target.gameObject.SetActive(false);
-                target = san;
-            }
-            else if (target == san)
-            {
-                //target.gameObject.SetActive(false);
-                target = ichi;
-            }
-
-            target.gameObject.SetActive(true);
-        }
+        target = ichi;
+        target.gameObject.SetActive(true);
         InstantiateEnemy();
-        SelectCharacter();
-
-
-
     }
 
     // plays a shake animation using an animator attached to the camera object, essentially altering the camera's position for the duration of  the animation
@@ -65,16 +39,6 @@ public class camerafollow : MonoBehaviour
     // returns the closest player in relation to a character - enemy or player
     public Transform ClosestPlayer(Transform self)
     {
-
-        if (ni.gameObject.activeSelf && san.gameObject.activeSelf)
-        {
-            var one = Vector2.Distance(self.position, ichi.position);
-            var two = Vector2.Distance(self.position, ni.position);
-            var three = Vector2.Distance(self.position, san.position);
-            if (one < two && one < three) return ichi; //one is the closest
-            if (three < two) return san; //one is not the closest, three is closer than two
-            return ni;
-        }
         return ichi;
     }
 
@@ -99,26 +63,11 @@ public class camerafollow : MonoBehaviour
     // determines and returns the player with the lowest health among the three defined player characters
     public Transform LowestHealthPlayer()
     {
-
-        if (ni.gameObject.activeSelf && san.gameObject.activeSelf)
-        {
-            var one = ichi.GetComponent<HealthController>().health;
-            var two = ni.GetComponent<HealthController>().health;
-            var three = san.GetComponent<HealthController>().health;
-            if (one < two && one < three) return ichi; //one is the lowest
-            if (three < two) return san; //one is not the lowest, three is lower than two
-            return ni;
-        }
         return ichi;
     }
 
     // determines lowest health player that is not the user, should only be called from a player character
-    public Transform LowestHealthOtherPlayer(Transform self)
-    {
-        if (self == ichi) { if (ni.GetComponent<HealthController>().health <= san.GetComponent<HealthController>().health) return ni; else return san; }
-        else if (self == ni) { if (ichi.GetComponent<HealthController>().health <= san.GetComponent<HealthController>().health) return ichi; else return san; }
-        else { if (ichi.GetComponent<HealthController>().health <= ni.GetComponent<HealthController>().health) return ichi; else return ni; }
-    }
+
 
     public void InstantiateEnemy()
     {
@@ -141,34 +90,5 @@ public class camerafollow : MonoBehaviour
         }
     }
 
-    public void SelectCharacter()
-    {
-        
-        if (Input.GetButtonDown("ichi"))
-        {
-            DisableCharacters();
-            ichi.gameObject.SetActive(true);
-            target = ichi;
-        }
-        if (Input.GetButtonDown("ni"))
-        {
-            DisableCharacters();
-            ni.gameObject.SetActive(true);
-            target = ni;
-        }
-        if (Input.GetButtonDown("san"))
-        {
-            DisableCharacters();
-            san.gameObject.SetActive(true);
-            target = san;
-        }
 
-    }
-
-    public void DisableCharacters()
-    {
-        ichi.gameObject.SetActive(false);
-        ni.gameObject.SetActive(false);
-        san.gameObject.SetActive(false);
-    }
 }
